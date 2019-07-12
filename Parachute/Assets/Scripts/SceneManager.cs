df969a19c3ff014f23857168b41b1bd8bacf60ef
ABMecik <35038370+ABMecik@users.noreply.github.com>
2019-07-12 13:01:25 +0300

    scene manager update

--- Parachute/Assets/Scripts/SceneManager.cs
+++ Parachute/Assets/Scripts/SceneManager.cs
@@ -5,4 +5,56 @@ using UnityEngine;
 public class SceneManager : MonoSingleton<SceneManager>
 {
 
+    enum Optional
+    {
+        Release,
+        Yes,
+        No
+    };
+
+    PlayerController player;
+    ObstaclePool pool;
+    GameObject ground;
+
+    [SerializeField] float SafeZoneRadius = 4f;
+    private Vector3 startPos;
+    private Vector3 currentPos;
+    private Vector3 endPos;
+    private List<float> lines;
+
+    [SerializeField] float minObsDistance = 2f;
+    [SerializeField] float maxObsDistance = 10f;
+
+    [SerializeField] Optional ObstacleCanMove = Optional.Release;
+
+    private void Start()
+    {
+        player = PlayerController.Instance;
+        pool = ObstaclePool.Instance;
+        ground = GameObject.FindGameObjectWithTag("Ground");
+
+        startPath();
+    }
+
+    public void startPath()
+    {
+
+        startPos = player.getPosition() - new Vector3(0, SafeZoneRadius, 0);
+        endPos = ground.transform.position + new Vector3(0, SafeZoneRadius, 0);
+        currentPos = startPos;
+
+        StartCoroutine("path");
+    }
+
+    public void endPath()
+    {
+        StopCoroutine("path");
+    }
+
+    IEnumerator path() {
+        while (true)
+        {
+
+        }
+    }
 }
