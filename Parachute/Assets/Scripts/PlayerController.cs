14fc329efc58bd19ded66099d76c7d78bc09b07c
emirhansenkal <46650883+emirhansenkal@users.noreply.github.com>
2019-07-12 11:42:38 +0300

    Cinemachine

--- Parachute/Assets/Scripts/PlayerController.cs
+++ Parachute/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoSingleton<PlayerController>
 {
-   
-   public Vector3 getPosition()
-   {
-       return transform.position;
-   }
+    #region  Variables
+
+
+
+    #endregion
+
+    #region Functions
+
+    public Vector3 getPosition()
+    {
+        return transform.position;
+    }
+
+    #endregion
+
 }
