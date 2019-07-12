051ae370ffda082d7388c23e087f72f8568ebe7d
ABMecik <35038370+ABMecik@users.noreply.github.com>
2019-07-12 11:55:38 +0300

    Bird Pool

--- Parachute/Assets/Scripts/GameManager.cs
+++ Parachute/Assets/Scripts/GameManager.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
-    #region Singelton
-    private GameManager() { }
 
-    public static GameManager Instance { get; private set; }
-
-    private void Awake()
-    {
-
-        Application.targetFrameRate = 60;
-
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            //Destroy(gameObject);
-        }
-
-    }
-    #endregion
 }
---
+++ Parachute/Assets/Scripts/ObstaclePool.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePool : MonoSingleton<GameManager>
+{
+    [System.Serializable]
+    public struct Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                obj.transform.SetParent(transform);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.Log(tag + "doesn't exist.");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+
+        return objectToSpawn;
+    }
+
+    internal GameObject SpawnFromPool(string tag, Vector3 position)
+    {
+
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.Log(tag + "doesn't exist.");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = findPrefabByTag(tag).transform.rotation;
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+
+        return objectToSpawn;
+    }
+
+    private GameObject findPrefabByTag(string tag)
+    {
+        foreach (Pool pool in pools)
+        {
+            if (pool.tag.Equals(tag))
+            {
+                return pool.prefab;
+            }
+        }
+
+        return null;
+    }
+}
