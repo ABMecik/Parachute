557dc7e808065916a48e658aeaf5b7cae7663a47
ABMecik <35038370+ABMecik@users.noreply.github.com>
2019-07-12 15:47:59 +0300

    lalala

--- Parachute/Assets/Scripts/Bird.cs
+++ Parachute/Assets/Scripts/Bird.cs
@@ -1,26 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    private List<float> lines;
+    PlayerController player;
+    SceneManager scene;
 
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        player = PlayerController.Instance;
+        scene = SceneManager.Instance;
+        lines = new List<float>(player.getLinePositionX());
     }
 
     private void OnBecameVisible()
     {
-        
-    }  
-
+        startFly();
+    }
     private void OnBecameInvisible()
     {
-        Debug.Log("agdsga");
         SceneManager.Instance.removeObs(gameObject);
     }
+
+
+    private void startFly()
+    {
+        gameObject.GetComponent<Rigidbody>().MovePosition(new Vector3(lines[UnityEngine.Random.Range(0, lines.Count)], transform.position.y+5, transform.position.z));
+    }
+
 }
